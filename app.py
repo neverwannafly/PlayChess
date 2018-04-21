ab99fcbc6842a79a66f78de1710d4e6d00855d4d
Shubham Anand <shubham_chess@live.com>
2018-04-21 22:10:45 +0530

    do necessary imports and setup

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms.validators import InputRequired, Length
+from wtforms import StringField, PasswordField
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from datetime import datetime
+from markupsafe import Markup
 
+from os.path import abspath
+from os import getcwd
 
 app = Flask(__name__)
+
+file_path = abspath(getcwd()) + '/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+file_path
+app.config['SECRET_KEY'] = 'Secret_Key'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
