756ed9333954a76237dbd164dd9465da763eeeb8
Shubham Anand <shubham_chess@live.com>
2018-04-25 20:24:42 +0530

    delete unused old env files

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from database import db
 
+app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return "working!"
 
 if __name__=='__main__':
     app.run(debug=True)
