1283e6d2ca2dfc641b4b5a2423adc7a6e45f5d97
Shubham Anand <shubham_chess@live.com>
2018-04-26 02:22:39 +0530

    add seperate database module

--- PlayChess/site/routes.py
+++ PlayChess/site/routes.py
@@ -3,10 +3,8 @@ import bcrypt as hash_pass
 
 mod = Blueprint('site', __name__, template_folder='templates')
 
-from pymongo import MongoClient
-
-client = MongoClient('mongodb://neverwannafly:Shubham123@ds151169.mlab.com:51169/chess_database')
-db = client.chess_database # Establishes connection to mlab database!
+from .. import database
+db = database.db
 
 @mod.route('/')
 def index():
