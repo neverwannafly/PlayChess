cfae31a3c08311c29f538664e4afc76eca54181c
Shubham Anand <shubham_chess@live.com>
2018-04-26 17:46:57 +0530

    add configuration file

---
+++ config.py
@@ -0,0 +1,6 @@
+# Contains username, pass for mongodb server here and also the secret key 
+configurations = {
+    '_USERNAME' : 'Neverwannafly',
+    '_PASSWORD' : 'Shubham123',
+    '_SECRET_KEY' : '5ae1bcf8f36d284005feddc9'
+}
