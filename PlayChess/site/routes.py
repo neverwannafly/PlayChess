7313323625b55f33a3e77891b4b2ebda06a25edd
Shubham Anand <shubham_chess@live.com>
2018-04-28 01:00:31 +0530

    add login route!

--- PlayChess/site/routes.py
+++ PlayChess/site/routes.py
@@ -1,4 +1,6 @@
 from flask import Blueprint, render_template, url_for, request, session, redirect
+from datetime import timedelta
+from functools import wraps
 import bcrypt as hash_pass
 import re as regex
 
@@ -16,54 +18,88 @@ db = database.db
 users = db.users # object pointing to users database!
 
 # Custom login_required decorator!
+def login_required(view_function):
+    @wraps(view_function)
+    def wrapper(*args, **kwargs):
+        username = session.get('username')
+        if username:
+            return view_function(*args, **kwargs)
+        else:
+            return redirect(url_for('site.login'))
+    return wrapper
+
+@mod.before_request
+def make_session_permanent():
+    session.permanent = True
+    mod.permanent_session_lifetime = timedelta(days=5)
+
+### View functions start ###
 
 @mod.route('/')
+@login_required
 def index():
-    return "You're in"
+    return "You're in" + session['username']
 
 @mod.route('/login', methods=['GET', 'POST'])
-def register():
-    if request.method=='POST':
-        existing_user = users.find_one({
-            'username' : request.form['username']
-        })
-        existing_email = users.find_one({
-            'email' : request.form['email']
-        })
-        if existing_email is None and existing_user is None:
-            valid_email = bool(regex.match(EMAIL_PATTERN_COMPILED, request.form['email']))
-            if valid_email:
-                if request.form['password']==request.form['confirm_password']:
-                    random_image = "use a random_image_url_generator!"
-                    hash_password = hash_pass.hashpw(request.form['password'], hash_pass.gensalt())
-                    new_user = classes.User(request.form['username'], hash_password, request.form['email'], random_image, request.form['first_name'], request.form['last_name'], 1200, users)
-                    new_user.addUserToDatabase()
-                    session['username'] = request.form['username']
-                    return redirect(url_for('site.index'))
-                else:
-                    return render_template('login.html', error_code=4)
-            else:
-                return render_template('login.html', error_code=3)
-        elif existing_email:
-            return render_template('login.html', error_code=2)
-        elif existing_user:
-            return render_template('login.html', error_code=1)
-
-    return render_template('login.html', error_code=0)
-
-@mod.route('/login', methods=['POST'])
 def login():
-    find_user = users.find_one({
-        'username' : request.form['username'],
-        'password' : request.form['password']
-    })
-    if find_user:
-        session['username'] = request.form['username']
-        return redirect(url_for('site.index'))
-    else: 
-        return render_template('login.html', error_code=-1)
+    if request.method=='POST':
+        find_user = users.find_one({
+            'username' : request.form['username'].lower(),
+        })
+        if find_user:
+            if hash_pass.hashpw(request.form['password'], find_user['password'])==find_user['password']:
+                session['username'] = request.form['username'].lower()
+                return redirect(url_for('site.index'))
+        else:
+            return render_template('login.html', error_code=-1, script=None)
+    return render_template('login.html', error_code=0, script=None)
 
-@mod.route('logout')
+@mod.route('/register', methods=['POST'])
+def register():
+    script = """
+    <script>
+        $(document).ready(function(){
+            $("#register-form").show();
+            $("#login-form").hide();
+        })
+    </script>
+    """
+    existing_user = users.find_one({
+        'username' : request.form['username'].lower()
+    })
+    existing_email = users.find_one({
+        'email' : request.form['email']
+    })
+    if existing_email is None and existing_user is None:
+        valid_email = bool(regex.match(EMAIL_PATTERN_COMPILED, request.form['email']))
+        if valid_email:
+            if request.form['password']==request.form['confirm_password']:
+                random_image = "use a random_image_url_generator!"
+                hash_password = hash_pass.hashpw(request.form['password'], hash_pass.gensalt())
+                new_user = classes.User(
+                    request.form['username'].lower(), 
+                    hash_password, 
+                    request.form['email'], 
+                    random_image, 
+                    request.form['first_name'], 
+                    request.form['last_name'], 
+                    1200, 
+                    users
+                )
+                new_user.addUserToDatabase()
+                session['username'] = request.form['username'].lower()
+                return redirect(url_for('site.index'))
+            else:
+                return render_template('login.html', error_code=4, script=script)
+        else:
+            return render_template('login.html', error_code=3, script=script)
+    elif existing_email:
+        return render_template('login.html', error_code=2, script=script)
+    elif existing_user:
+        return render_template('login.html', error_code=1, script=script)
+
+@mod.route('/logout')
+@login_required
 def logout():
     session.pop('username')
-    return redirect(url_for('site.register'))
+    return redirect(url_for('site.login'))
