b6fe1be33128f45928cbee662d158fd578e62bcb
Shubham Anand <shubham_chess@live.com>
2018-04-30 20:07:42 +0530

    add admin routes

--- PlayChess/admin/routes.py
+++ PlayChess/admin/routes.py
@@ -1,6 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, url_for, request, session, redirect
 
-mod = Blueprint('admin', __name__, template_folder='templates')
+mod = Blueprint('admin', __name__, template_folder='admin_templates')
 
 from .. import database
 db = database.db
+
+@mod.route('/')
+def index():
+    return render_template('admin_login.html')
