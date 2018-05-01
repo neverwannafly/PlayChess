622a99bc221f31aa1c157d57e960bd9b953013bd
Shubham Anand <shubham_chess@live.com>
2018-05-01 16:55:50 +0530

    create new class member create_new_admin to create a new admin

--- PlayChess/__init__.py
+++ PlayChess/__init__.py
@@ -11,5 +11,12 @@ app = Flask(__name__)
 from PlayChess.site.routes import mod
 from PlayChess.admin.routes import mod
 
+# makes an instance of admin class that can be used to add new admins!
+
+from PlayChess.admin.routes import new_admin
+create_new_admin = new_admin
+
+# Register the blueprints!
+
 app.register_blueprint(site.routes.mod)
 app.register_blueprint(admin.routes.mod, url_prefix='/admin')
