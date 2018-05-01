b41939c30bc9aa10946485c5765827929fce7f3b
Shubham Anand <shubham_chess@live.com>
2018-05-01 15:47:32 +0530

    add delete and create user functionality to class based admin design

--- PlayChess/admin/classes.py
+++ PlayChess/admin/classes.py
@@ -1,3 +1,6 @@
+# Add necessary imports!
+import bcrypt as hash_pass
+
 class Admin:
 
     # A class to create an admin user instant!
@@ -27,5 +30,24 @@ class Admin:
             'admin_password': admin_password
         })
 
-    # creates a new user to be added to database!
-    def createUser():
+    # Lets the admin create a new user to be added to database!
+    def createUser(self, username, password, email, image, first_name, last_name):
+        self.db.users.insert_one({
+            "first_name" : first_name,
+            "last_name" : last_name,
+            "username" : username,
+            "email" : email,
+            "password" : password,
+            "image" : image,
+            "rating" : 1200,
+            "isUserVerified" : True
+        })
+    
+    # Lets the admin delete a user!
+    def deleteUser(self, username):
+        self.db.users.delete_one({
+            'username': username
+        })
+
+    # Add editing capabilities using Ajax later!
+    ###### EDITING METHODS ######
