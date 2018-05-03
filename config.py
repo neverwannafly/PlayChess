f4ff3e09c7ec70bbc44f18c7dfcbf5e017fefe8b
Shubham Anand <shubham_chess@live.com>
2018-05-03 21:07:50 +0530

    update secret_key

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Contains username, pass for mongodb server here and also the secret key 
 configurations = {
-    '_SECRET_KEY' : '5ae1bcf8f36d284005feddc9'
+    '_SECRET_KEY' : 'i2MD75qiYd4CJuqTRHB4'
 }
