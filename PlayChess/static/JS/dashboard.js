f225c371f3d025cdf4e2afb2c8823e5095e48d48
Shubham Anand <shubham_chess@live.com>
2018-05-06 05:08:33 +0530

    add js to dashboard.html

--- PlayChess/static/JS/dashboard.js
+++ PlayChess/static/JS/dashboard.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-    
-})
+    $("tr").hover(function(){
+        $(this).children(".normal-cell").css("background-color", "pink");
+        $(this).children(".hidden-cell").css("visibility", "visible");
+    }, function() {
+        $(this).children(".hidden-cell").css("visibility", "hidden");
+        $(this).children(".normal-cell").css("background-color", "white");
+    });
+
+    $(".deleteButton").click(function(){
+        var username = $(this).val();
+        $(".hidden-form").val(username);
+        $("#exampleModalLabel").text("Delete user " + username + " ?");
+    })
+});
