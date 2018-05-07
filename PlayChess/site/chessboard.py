eb3a40a4a4f29f3b5a266dfe58eb72611af247db
Shubham Anand <shubham_chess@live.com>
2018-05-07 07:09:07 +0530

    add layout for other pieces

--- PlayChess/site/chessboard.py
+++ PlayChess/site/chessboard.py
@@ -5,9 +5,9 @@ class Piece:
     # Initialises the basic component of every piece
     def __init__(self):
         self.points = 0
-        self.legal_moves = []
         self.current_position = ""
         self.color = ""
+        self.legal_moves = []
         self.isPieceCaptured = False
     
     def makeMove(self, move):    
@@ -17,22 +17,76 @@ class Piece:
         self.isPieceCaptured = True
     
 class Pawn(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 1
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 class Knight(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 3
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 class Bishop(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 3
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 class Rook(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 5
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 class Queen(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 9
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 class King(Piece):
-    pass
+    def __init__(self, current_position, color):
+        super.__init__()
+        self.points = 100
+        self.current_position = current_position
+        self.color = color
+        self.legal_moves = self.generate_legal_moves(current_position)
+        self.isPieceCaptured = False
+
+    def generate_legal_moves(self, position):
+        pass
 
 # Defines the final layout of the chessboard!
 class Chessboard():
