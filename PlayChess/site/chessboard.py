58f10549665e834561dbfe98645ff0999f8af354
Shubham Anand <shubham_chess@live.com>
2018-05-07 07:00:24 +0530

    make basic chessboard layout

--- PlayChess/site/chessboard.py
+++ PlayChess/site/chessboard.py
@@ -1,10 +1,39 @@
 # Define a class for chessboard pieces with necessary members and methods.
 # This would serve as the parent class for other pieces.
 
-class Pieces:
-    def __init__():
-        points = 0
-        legal_moves = []
-        current_position = ""
-        color = 
-        isPieceCaptured
+class Piece:
+    # Initialises the basic component of every piece
+    def __init__(self):
+        self.points = 0
+        self.legal_moves = []
+        self.current_position = ""
+        self.color = ""
+        self.isPieceCaptured = False
+    
+    def makeMove(self, move):    
+        self.current_position = move
+    
+    def pieceCaptured(self):
+        self.isPieceCaptured = True
+    
+class Pawn(Piece):
+    pass
+
+class Knight(Piece):
+    pass
+
+class Bishop(Piece):
+    pass
+
+class Rook(Piece):
+    pass
+
+class Queen(Piece):
+    pass
+
+class King(Piece):
+    pass
+
+# Defines the final layout of the chessboard!
+class Chessboard():
+    pass
