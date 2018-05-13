9497b42fa32139eeee720c852c70150412f2e488
Shubham Anand <shubham_chess@live.com>
2018-05-13 14:15:36 +0530

    add update js

--- PlayChess/static/JS/dashboard.js
+++ PlayChess/static/JS/dashboard.js
@@ -13,4 +13,25 @@ $(document).ready(function(){
         $("#exampleModalLabel").text("Delete user " + username + " ?");
     })
 
+    var counter = 0;
+    $(".editButton").click(function(){
+        var button_class = "." + $(this).val();
+        if (counter%2==0) {
+            $(button_class).find("div").css("display", "none");
+            $(button_class).find(".hidden-form").css("display", "block");
+            $(button_class).find(".deleteButton").css("display", "none");
+            $(button_class).find(".editButton").text("Update");
+            //$(button_class).find(".editButton").attr("type", "submit");
+        }
+        else {
+            $(button_class).find("div").css("display", "block");
+            $(button_class).find(".hidden-form").css("display", "none");
+            $(button_class).find(".deleteButton").css("display", "block");
+            $(button_class).find(".editButton").text("Edit");
+            //$(button_class).find(".editButton").attr("type", "button");
+        }
+        
+        return counter += 1;
+    });
+
 });
