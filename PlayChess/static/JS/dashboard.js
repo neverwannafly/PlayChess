b79bafce89e355999ffaef1ee5eea2c542fffa5f
Shubham Anand <shubham_chess@live.com>
2018-05-15 16:02:47 +0530

    add js to handle update operation using AJAX

--- PlayChess/static/JS/dashboard.js
+++ PlayChess/static/JS/dashboard.js
@@ -9,30 +9,62 @@ $(document).ready(function(){
 
     $(".deleteButton").click(function(){
         var username = $(this).val();
-        $(".hidden-form").val(username);
+        $("#exampleModal").find(".hidden-form").val(username);
         $("#exampleModalLabel").text("Delete user " + username + " ?");
     })
 
-    var counter = 0;
     $(".editButton").click(function(event){
         var button_class = "." + $(this).val();
-        if (counter%2==0) {
-            $(button_class).find("div").hide();
-            $(button_class).find(".hidden-form").show();
-            $(button_class).find(".deleteButton").hide();
-            $(button_class).find(".editButton").text("Update");
-            $(button_class).find(".editButton").attr("type", "submit");
-            event.preventDefault();
-        }
-        else {
-            $(button_class).find(".hidden-form").hide();
-            $(button_class).find("div").show();
-            $(button_class).find(".deleteButton").show();
-            $(button_class).find(".editButton").text("Edit");
-            $(button_class).find(".editButton").attr("type", "button");
-        }
-        
-        return counter += 1;
+        editButtonPressed(button_class, event);
     });
 
+    $(".updateButton").click(function(event){
+        var button_class = "." + $(this).val();
+        updateButtonPressed(button_class, event);
+    })
+
+    $("form").on("submit", function(event){
+        var username_class = "." + $(this).attr("id").split('-')[0];
+        $.ajax({
+            data : {
+                username : $(username_class).find("input[name='username']").val(),
+                name : $(username_class).find("input[name='name']").val(),
+                email : $(username_class).find("input[name='email']").val(),
+                rating : $(username_class).find("input[name='rating']").val(),
+                authentication : $(username_class).find("select[name='authentication']").val(),
+                image : $(username_class).find("input[name='image']").val()
+            },
+            type : "POST",
+            url : "update"
+        })
+        .done(function(data){
+            var forms = $(username_class).find(".hidden-form");
+            var divs = $(username_class).find(".detail");
+
+            var counter = 0;
+            for (element in data) {
+                forms.eq(counter).val(data[element]);
+                divs.eq(counter).text(data[element]);
+                counter += 1;
+            }
+        })
+        event.preventDefault();
+    })
+
 });
+
+function editButtonPressed(button, event) {
+    $(button).find(".detail").hide();
+    $(button).find(".deleteButton").hide();
+    $(button).find(".editButton").hide();
+    $(button).find(".hidden-form").show();
+    $(button).find(".updateButton").show();
+}
+
+function updateButtonPressed(button, event) {
+    $(button).find(".hidden-form").hide();
+    $(button).find(".updateButton").hide();
+    $(button).find(".detail").show();
+    $(button).find(".deleteButton").show();
+    $(button).find(".editButton").show();
+}
