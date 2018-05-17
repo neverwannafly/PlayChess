3ed8ff86fc0b508ca4ce2c27321244a258098eed
Shubham Anand <shubham_chess@live.com>
2018-05-17 20:54:46 +0530

    add class to make chess boards

--- PlayChess/site/chessboard.py
+++ PlayChess/site/chessboard.py
@@ -1,96 +1,66 @@
-# Defines a blank class to identify blank sqaures on chessboard!
-
-class Blank:
+class Chessboard:
     def __init__(self):
-        self.label = "Blank"
+        self.board = ""
+        self.id = 0
 
-    def returnLabel(self):
-        return self.label
+    def swapBoard(self):
+        if self.id == 2:
+            self.id =1
+            return self.createBoardForBlack()
+        else:
+            self.id = 2
+            return self.createBoardForWhite()
 
-# Define a class for chessboard pieces with necessary members and methods.
-# This would serve as the parent class for other pieces.
+    def createBoardForBlack(self):
+        self.board = ""
+        for row in range(8):
+            self.board = self.board + "<tr>"
+            first = "h"
+            for column in range(8):
+                if (column+row)%2==0:
+                    self.board = self.board + """
+                    <td>
+                        <div class="square dark" id="#{id}">
 
-class Piece:
-    # Initialises the basic component of every piece
-    def __init__(self):
-        self.points = 0
-        self.current_position = ""
-        self.color = ""
-        self.legal_moves = []
-        self.label = Piece
-    
-    def makeMove(self, move):    
-        self.current_position = move
+                        </div>
+                    </td>
+                    """.format(id=first+str(row+1))
+                else:
+                    self.board = self.board + """
+                    <td>
+                        <div class="square light" id="#{id}">
 
-    def returnLabel(self):
-        return self.label
-    
-class Pawn(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 1
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
+                        </div>
+                    </td>
+                    """.format(id=first+str(row+1))
+                first = chr(ord(first)-1)
+            self.board = self.board + "</tr>"
+        self.id = 1
+        return self.board
 
-    def generate_legal_moves(self, position):
-        pass
+    def createBoardForWhite(self):
+        self.board = ""
+        for row in range(8):
+            first = "a"
+            self.board = self.board + "<tr>"
+            for column in range(8):
+                if (column+row)%2==0:
+                    self.board = self.board + """
+                    <td>
+                        <div class="square light" id="#{id}">
 
-class Knight(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 3
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
+                        </div>
+                    </td>
+                    """.format(id=first+str(8-row))
+                else:
+                    self.board = self.board + """
+                    <td>
+                        <div class="square dark" id="#{id}">
 
-    def generate_legal_moves(self, position):
-        pass
-
-class Bishop(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 3
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
-
-    def generate_legal_moves(self, position):
-        pass
-
-class Rook(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 5
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
-
-    def generate_legal_moves(self, position):
-        pass
-
-class Queen(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 9
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
-
-    def generate_legal_moves(self, position):
-        pass
-
-class King(Piece):
-    def __init__(self, current_position, color):
-        super.__init__()
-        self.points = 100
-        self.current_position = current_position
-        self.color = color
-        self.legal_moves = self.generate_legal_moves(current_position)
-
-    def generate_legal_moves(self, position):
-        pass
-
-# Defines the final layout of the chessboard!
-class Chessboard():
-    pass
+                        </div>
+                    </td>
+                    """.format(id=first+str(8-row))
+                first = chr(ord(first)+1)
+            self.board = self.board + "</tr>"
+        self.id = 2
+        return self.board
