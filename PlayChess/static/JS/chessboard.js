14c52db7d81a3f56708cd5c592f73e1a635bb6ab
Shubham Anand <shubham_chess@live.com>
2018-05-31 21:53:44 +0530

    add text interface to make moves [TEST]

--- PlayChess/static/JS/chessboard.js
+++ PlayChess/static/JS/chessboard.js
@@ -10,19 +10,4 @@ $(document).ready(function(){
             $("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
         })
     });
-    $("td").click(function(){
-        var init_cell_id = $(this).find("div").attr("id");
-        $("td").click(function(){
-            var final_cell_id = $(this).find("div").attr("id");
-            var parameter = init_cell_id + "-" + final_cell_id;
-            $.ajax({
-                type: "GET",
-                url: "makemove/" + parameter
-            })
-            .done(function(data){
-                $("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
-            })
-        })
-        // filter: brightness(70%);
-    })
 });
--- PlayChess/static/JS/index.js
+++ PlayChess/static/JS/index.js
@@ -4,4 +4,19 @@ $(document).ready(function() {
     $(".button").click(function(){
         alert("Under development!");
     })
+    $(".makeMove").click(function(){
+        var move = $("#makemoveinput").val();
+        console.log(move);
+        $.ajax({
+            type: "GET",
+            url: "makemove/"+move,
+            error : function(xhr, status, error) {
+                alert("Please enter a valid move!");
+             },
+        })
+        .done(function(data){
+            $("iframe").contents().find("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
+            $("#makemoveinput").val("");
+        })
+    })
 });
