7262c1611050214e561adccac31eef0df0e6d347
Shubham Anand <shubham_chess@live.com>
2018-06-03 20:47:20 +0530

    add enter button method to make moves

--- PlayChess/static/JS/index.js
+++ PlayChess/static/JS/index.js
@@ -5,18 +5,26 @@ $(document).ready(function() {
         alert("Under development!");
     })
     $(".makeMove").click(function(){
-        var move = $("#makemoveinput").val();
-        console.log(move);
-        $.ajax({
-            type: "GET",
-            url: "makemove/"+move,
-            error : function(xhr, status, error) {
-                alert("Please enter a valid move!");
-             },
-        })
-        .done(function(data){
-            $("iframe").contents().find("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
-            $("#makemoveinput").val("");
-        })
+        sendAjaxRequest();
+    })
+    $("#makemoveinput").keyup(function(e){
+        if (e.which==13) {
+            sendAjaxRequest();
+        }
     })
 });
+
+function sendAjaxRequest() {
+    var move = $("#makemoveinput").val();
+    $.ajax({
+        type: "GET",
+        url: "makemove/"+move,
+        error : function(xhr, status, error) {
+            alert("Please enter a valid move!");
+            },
+    })
+    .done(function(data){
+        $("iframe").contents().find("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
+        $("#makemoveinput").val("");
+    });
+}
