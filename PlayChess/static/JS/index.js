5c14f28938afff47c0cf8b17089716d54444297c
Shubham Anand <shubham_chess@live.com>
2018-06-05 23:15:02 +0530

    deprecate text based interface

--- PlayChess/static/JS/index.js
+++ PlayChess/static/JS/index.js
@@ -4,27 +4,4 @@ $(document).ready(function() {
     $(".button").click(function(){
         alert("Under development!");
     })
-    $(".makeMove").click(function(){
-        sendAjaxRequest();
-    })
-    $("#makemoveinput").keyup(function(e){
-        if (e.which==13) {
-            sendAjaxRequest();
-        }
-    })
 });
-
-function sendAjaxRequest() {
-    var move = $("#makemoveinput").val();
-    $.ajax({
-        type: "GET",
-        url: "makemove/"+move,
-        error : function(xhr, status, error) {
-            alert("Please enter a valid move!");
-            },
-    })
-    .done(function(data){
-        $("iframe").contents().find("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
-        $("#makemoveinput").val("");
-    });
-}
