452033950bca336e79c630a8c4d6159fc7920760
Shubham Anand <shubham_chess@live.com>
2018-06-05 20:51:57 +0530

    add loader to resend link

--- PlayChess/static/JS/verify.js
+++ PlayChess/static/JS/verify.js
@@ -2,11 +2,13 @@
 
 $(document).ready(function(){
     $("#verify").click(function(){
+        $(".loader").show();
         $.ajax({
             type: "POST",
             url: "retry"
         })
         .done(function(data){
+            $(".loader").hide();
             var html = `
             <div class="alert alert-success alert-dismissible fade show">
             <button type="button" class="close" data-dismiss="alert">&times;</button>
