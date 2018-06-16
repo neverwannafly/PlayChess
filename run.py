7898cec0e6345d380a2748f4c573de13a71c12cf
Shubham Anand <shubham_chess@live.com>
2018-06-16 22:14:35 +0530

    shift configs to PlayChess module

--- run.py
+++ run.py
@@ -1,8 +1,4 @@
 from PlayChess import app
-from config import configurations
-
-app.secret_key = configurations['_SECRET_KEY']
-app.config['JSON_SORT_KEYS'] = False
 
 if __name__ == '__main__':
     app.run(debug=True)
