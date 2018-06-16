7e49344974aa889a54e66fee9d477503ae69a104
Shubham Anand <shubham_chess@live.com>
2018-06-17 00:15:10 +0530

    make dicrete testing configs

--- PlayChess/__init__.py
+++ PlayChess/__init__.py
@@ -12,7 +12,9 @@ app = Flask(__name__)
 # Add configurations
 
 app.secret_key = configurations['_SECRET_KEY']
-app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = configurations['JSON_AUTO_SORT']
+app.config['TEST_USERNAME'] = configurations['TEST_USERNAME']
+app.config['TEST_PASSWORD'] = configurations['TEST_PASSWORD']
 
 from PlayChess.site.routes import mod
 from PlayChess.admin.routes import mod
--- PlayChess/config.py
+++ PlayChess/config.py
@@ -1,4 +1,7 @@
 # Contains username, pass for mongodb server here and also the secret key 
 configurations = {
-    '_SECRET_KEY' : 'i2MD75qiYd4CJuqTRHB4'
+    '_SECRET_KEY' : "i2MD75qiYd4CJuqTRHB4",
+    'JSON_AUTO_SORT': False,
+    'TEST_USERNAME': "test",
+    'TEST_PASSWORD': "tets",
 }
