8f1c8037f3d0b9de88685d82bd0dc76f2427223e
Shubham Anand <shubham_chess@live.com>
2018-06-17 00:15:32 +0530

    add unified method to login and do tests for protected views

--- Tests/client.py
+++ Tests/client.py
@@ -6,3 +6,6 @@ def client():
     PlayChess.app.config['TESTING'] = True
     client = PlayChess.app.test_client()
     yield client
+
+def login(client):
+    return_login_status = client.post('/login', data={"username": "unittest", "password": "unittest"})
