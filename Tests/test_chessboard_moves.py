874ea8a6360b56af9c842b66fa482614af034106
Shubham Anand <shubham_chess@live.com>
2018-07-10 21:35:45 +0530

    add incomplete tests for castling

--- Tests/test_chessboard_moves.py
+++ Tests/test_chessboard_moves.py
@@ -18,3 +18,11 @@ def test_legal_pawn_moves(client):
     for test_case in corner_pawn_moves:
         assert test_case.status_code==200
     # Add tests for enpassant later
+
+def test_castling(client):
+    login(client)
+    # vestigial moves to clear away first rank to enable castling
+    client.get('/makemove/e2-e4')
+    client.get('/makemove/e7-e5')
+    client.get('/makemove/g1-f3')
+    client.get('/makemove/g8-f6')
