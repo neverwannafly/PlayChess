b8b3ec001516f66ab90ec4b4ac70492da864a93c
Shubham Anand <shubham_chess@live.com>
2018-07-15 21:47:39 +0530

    make secret key random

--- PlayChess/config.py
+++ PlayChess/config.py
@@ -1,6 +1,8 @@
+import os
+
 # Contains username, pass for mongodb server here and also the secret key 
 configurations = {
-    '_SECRET_KEY' : "i2MD75qiYd4CJuqTRHB4",
+    '_SECRET_KEY' : os.urandom(64),
     'JSON_AUTO_SORT': False,
     'TEST_USERNAME': "test",
     'TEST_PASSWORD': "tets",
