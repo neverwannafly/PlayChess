42be0c5f1254ea15007533dc12894641e1ad9973
Shubham Anand <shubham_chess@live.com>
2018-08-03 17:02:11 +0530

    follow self running function design

--- PlayChess/static/JS/chessboard.js
+++ PlayChess/static/JS/chessboard.js
@@ -1,42 +1,49 @@
 "use strict";
 
-$(document).ready(function(){
-    $(".flipButton").on('click', function(){
-        $.ajax({
-            type: "GET",
-            url: "board/flip"
-        })
-        .done(function(data){
-            $("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
-        });
-    });
-
-    var initial_pos, final_pos;
-
-    $(document).on('click', '.square', function(e){
-        var target = $(e.target), article;
-        if(incrementClick()%2!=0) {
-            initial_pos = $(target).attr('id');
-        }
-        else {
-            final_pos = $(target).attr('id');
-            var url = "makemove/" + initial_pos + "-" + final_pos;
+(function(){
+    $(document).ready(function(){
+        $(".flipButton").on('click', function(){
             $.ajax({
-                url: url,
-                error : function(xhr, status, error) {
-                    console.log("Please enter valid move!");
-                },
+                type: "GET",
+                url: "board/flip"
             })
             .done(function(data){
                 $("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
-            })
-        }
-    });
-});
+            });
+        });
 
-var incrementClick = (function(){
-    var counter = 0;
-    return function() {
-        return counter += 1;
+        var initial_pos, final_pos;
+
+        $(document).on('click', '.square', function(e){
+            var target = $(e.target), article;
+            if(incrementClick()%2!=0) {
+                initial_pos = $(target).attr('id');
+            }
+            else {
+                final_pos = $(target).attr('id');
+                var url = "makemove/" + initial_pos + "-" + final_pos;
+                $.ajax({
+                    url: url,
+                    error : function(xhr, status, error) {
+                        console.log(xhr, status, error);
+                        console.log("Please enter valid move!");
+                    },
+                })
+                .done(function(data){
+                    $("tbody").replaceWith("<tbody>"+data.board+"</tbody>");
+                })
+            }
+        });
+    });
+
+    function make_move(initial_pos, final_pos) {
+
     }
-}) ();
+
+    var incrementClick = (function(){
+        var counter = 0;
+        return function() {
+            return counter += 1;
+        }
+    })();
+})();
