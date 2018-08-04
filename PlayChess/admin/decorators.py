3ca6368b46185905ce7e699644d728abfe23b7ca
Shubham Anand <shubham_chess@live.com>
2018-08-04 17:43:12 +0530

    create new file to hold decorators

---
+++ PlayChess/admin/decorators.py
@@ -0,0 +1,27 @@
+from flask import url_for, session, redirect
+from functools import wraps
+
+# login_required and logout_required decorators that ensure certain url's confront to 
+# respective wanted behaviors!
+
+# Custom login_required decorator
+def login_required(view_function):
+    @wraps(view_function)
+    def wrapper(*args, **kwargs):
+        admin_username = session.get('admin_username')
+        if admin_username:
+            return view_function(*args, **kwargs)
+        else:
+            return redirect(url_for('admin.login'))
+    return wrapper
+
+# logout required decorator to access admin login!
+def logout_required(view_function):
+    @wraps(view_function)
+    def wrapper(*args, **kwargs):
+        admin_username = session.get('admin_username')
+        if admin_username:
+            return redirect(url_for('admin.dashboard'))
+        else:
+            return view_function(*args, **kwargs)
+    return wrapper
