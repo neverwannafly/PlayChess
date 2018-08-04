ffbef3cbcab41df33d1e16d766aefd3206e486ee
Shubham Anand <shubham_chess@live.com>
2018-08-04 12:08:18 +0530

    restrict hover effects on only pieces

--- PlayChess/static/JS/chessboard.js
+++ PlayChess/static/JS/chessboard.js
@@ -14,8 +14,8 @@
 
         var initial_pos, final_pos;
 
-        $(document).on('click', '.square', function(e){
-            const target = $(e.target);
+        $(document).on('click', '.square', (event) => {
+            const target = $(event.target);
             if(incrementClick()%2!=0) {
                 initial_pos = $(target).attr('id');
             }
@@ -35,15 +35,29 @@
                 });
             }
         });
+
+        $("td").hover(mouseIn, mouseOut);
+
     });
 
     function make_move(changes) {
         for (var i=0; i<changes.length; i++) {
             const square_id = "#" + changes[i]['pos'];
             const square_class = changes[i]['class'];
-            $(square_id).removeClass("white-K white-Q white-R white-B white-N white-p black-K black-Q black-R black-B black-N black-p");
+            $(square_id).removeClass("white-K white-Q white-R white-B white-N white-p black-K black-Q black-R black-B black-N black-p none-_");
             $(square_id).addClass(square_class);
         }
+    }
+
+    function mouseIn() {
+        const square = $(this).find('.square');
+        if (!$(square).hasClass("none-_") || $(square).hasClass("active-square")) {
+            square.addClass("highlighted-cell");
+        }
+    }
+
+    function mouseOut() {
+        $(this).find('.square').removeClass("highlighted-cell");
     }
 
     var incrementClick = (function(){
