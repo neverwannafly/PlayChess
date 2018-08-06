1034383d9594d5593b9de765a5759676215ce2b5
Shubham Anand <shubham_chess@live.com>
2018-08-06 12:58:45 +0530

    Make messages class for comprehensive messages

---
+++ PlayChess/site/message.py
@@ -0,0 +1,16 @@
+## Allows you to make your messages more comprehensive
+
+class ComprehensiveMessage:
+    
+    def __init__(self, message, code, success=False):
+        self.message = message
+        self.error_code = error_code
+
+    def message(self):
+        return self.message
+
+    def code(self):
+        return self.error_code
+
+    def is_success(self):
+        return self.success
