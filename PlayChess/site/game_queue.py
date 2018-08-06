4b9a30f9080a984618cec08707b8af17312277c3
Shubham Anand <shubham_chess@live.com>
2018-08-06 12:58:32 +0530

    complete GameQueue class

--- PlayChess/site/game_queue.py
+++ PlayChess/site/game_queue.py
@@ -1,8 +1,36 @@
 ## A rudimentary game queueing class.
 
+from .message import ComprehensiveMessage
+
 class GameQueue:
-    
+
     def __init__(self):
-        
         self._queue = []
-        self._limit = 2 
+
+    def _add(self, player):
+        if len(self._queue) == 0:
+            self._queue.append(player)
+            return ComprehensiveMessage("Player Added Successfully!", 1, success=True)
+
+        elif len(self._queue) == 1:
+            self._queue.append(player)
+            return ComprehensiveMessage("2 Players in the Queue!", 2, success=True)
+
+        else:
+            return ComprehensiveMessage("Game Queue Full!", -1)
+
+    def _generate_url(self):
+        self._queue.sort()
+        url = "{0}-{1}".format(_queue[0], _queue[1])
+        return url
+
+    def add_to_queue(self, player):
+        addition = self._add(player)
+        if addition.code == 2:
+            print(addition.message)
+            url = self._generate_url()
+            del self._queue[:]
+            return url
+
+        print(addition.message)
+        return None
