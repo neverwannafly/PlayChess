509f825c906b2a58d630d66917126d29e687db7d
Shubham Anand <shubham_chess@live.com>
2018-08-07 16:14:55 +0530

    enforce type checking

--- PlayChess/site/game_queue.py
+++ PlayChess/site/game_queue.py
@@ -8,16 +8,22 @@ class GameQueue:
         self._queue = []
 
     def _add(self, player):
-        if len(self._queue) == 0:
-            self._queue.append(player)
-            return ComprehensiveMessage("Player Added Successfully!", 1, success=True)
+        
+        if player not in self._queue:
 
-        elif len(self._queue) == 1:
-            self._queue.append(player)
-            return ComprehensiveMessage("2 Players in the Queue!", 2, success=True)
+            if len(self._queue) == 0:
+                self._queue.append(player)
+                return ComprehensiveMessage("Player Added Successfully!", 1, success=True)
+
+            elif len(self._queue) == 1:
+                self._queue.append(player)
+                return ComprehensiveMessage("2 Players in the Queue!", 2, success=True)
+
+            else:
+                return ComprehensiveMessage("Game Queue Full!", 0)
 
         else:
-            return ComprehensiveMessage("Game Queue Full!", 0)
+            return ComprehensiveMessage("Player Already in Game!", 3)
 
     def _generate_url(self):
         self._queue.sort()
@@ -29,8 +35,8 @@ class GameQueue:
         if addition.code == 2:
             print(addition.message)
             url = self._generate_url()
-            del self._queue[:]
             addition.info = url
+            del self._queue[:]
 
         return addition
 
