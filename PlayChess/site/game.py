a02f7f63078de0478372948b8d7027e1b3365760
Shubham Anand <shubham_chess@live.com>
2018-08-07 18:13:00 +0530

    add player assignment method

--- PlayChess/site/game.py
+++ PlayChess/site/game.py
@@ -1,5 +1,7 @@
 """Contains a Game class that can handle chessgames b/w two players"""
 
+import random
+
 from .chessboard import Chessboard
 
 class Game:
@@ -8,9 +10,15 @@ class Game:
         self.chessboard = Chessboard()
 
         # Player1 would be white and Player2 would be black!
-
-        self.player1 = game_name.split('-')[0]
-        self.player2 = game_name.split('-')[1]
+        self.player1 = None
+        self.player2 = None
         self.spectators = []
 
         self.game_name = game_name
+        self.assign_players()
+
+    def assign_players(self):
+        index = random.randint(0,1)
+        player1, player2 = self.game_name.split('-')[index], self.game_name.split('-')[int(not index)]
+        self.player1 = player1
+        self.player2 = player2
