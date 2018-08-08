3437902588b151ecb131a53439ed261b143fe453
Shubham Anand <shubham_chess@live.com>
2018-08-08 12:10:49 +0530

    make basic chat app

--- PlayChess/chat/routes.py
+++ PlayChess/chat/routes.py
@@ -1,3 +1,15 @@
 from flask import Blueprint, render_template, url_for, request, session, redirect, jsonify
+from flask_socketio import send
 
 mod = Blueprint('chat', __name__, template_folder='templates')
+
+from .. import socketio
+
+@mod.route('/')
+def message():
+    return render_template('global-chat.html')
+
+@socketio.on('message')
+def handleMessage(msg):
+	print('Message: ' + msg)
+	send(msg, broadcast=True)
--- PlayChess/static/JS/chat.js
+++ PlayChess/static/JS/chat.js
@@ -4,7 +4,20 @@
 
     $(document).ready(function(){
 
-        
+        const url = window.location.origin;
+        var socket = io.connect(url);
+
+        socket.on('connect', function() {
+            socket.send('User has connected!');
+        });
+        socket.on('message', function(msg) {
+            $("#messages").append('<li>'+msg+'</li>');
+            console.log('Received message');
+        });
+        $('#sendbutton').on('click', function() {
+            socket.send($('#myMessage').val());
+            $('#myMessage').val('');
+        });
 
     });
 
