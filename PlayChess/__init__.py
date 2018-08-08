482ed6eba7c90a2e43362228abcb7d89309823fa
Shubham Anand <shubham_chess@live.com>
2018-08-08 11:40:45 +0530

    configure flask app for socketio

--- PlayChess/__init__.py
+++ PlayChess/__init__.py
@@ -5,6 +5,8 @@ Makes the web application modular!
 """
 
 from flask import Flask
+from flask_socketio import SocketIO, send
+
 from .config import configurations
 
 app = Flask(__name__)
@@ -19,6 +21,7 @@ app.config['TEST_PASSWORD'] = configurations['TEST_PASSWORD']
 from PlayChess.site.routes import mod
 from PlayChess.admin.routes import mod
 from PlayChess.blog.routes import mod
+from PlayChess.chat.routes import mod
 
 # makes an instance of admin class that can be used to add new admins!
 
@@ -29,6 +32,11 @@ from PlayChess.admin.admins import create_admin
 app.register_blueprint(site.routes.mod)
 app.register_blueprint(admin.routes.mod, url_prefix='/admin')
 app.register_blueprint(blog.routes.mod, url_prefix='/blog')
+app.register_blueprint(chat.routes.mod, url_prefix='/chat')
+
+## Socket IO connection for chat and playing chess game.
+
+socketio = SocketIO(app)
 
 # Makes chess board easily accessible thorugh terminal
 
