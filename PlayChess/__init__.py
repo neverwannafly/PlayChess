8e2cf94be5d6556a4562e10d215c82d0cbb28aae
Shubham Anand <shubham_chess@live.com>
2018-08-08 12:10:38 +0530

    fix socketio import

--- PlayChess/__init__.py
+++ PlayChess/__init__.py
@@ -5,7 +5,7 @@ Makes the web application modular!
 """
 
 from flask import Flask
-from flask_socketio import SocketIO, send
+from flask_socketio import SocketIO
 
 from .config import configurations
 
@@ -17,6 +17,9 @@ app.secret_key = configurations['_SECRET_KEY']
 app.config['JSON_SORT_KEYS'] = configurations['JSON_AUTO_SORT']
 app.config['TEST_USERNAME'] = configurations['TEST_USERNAME']
 app.config['TEST_PASSWORD'] = configurations['TEST_PASSWORD']
+
+## Socket IO connection for chat and playing chess games.
+socketio = SocketIO(app)
 
 from PlayChess.site.routes import mod
 from PlayChess.admin.routes import mod
@@ -33,10 +36,6 @@ app.register_blueprint(site.routes.mod)
 app.register_blueprint(admin.routes.mod, url_prefix='/admin')
 app.register_blueprint(blog.routes.mod, url_prefix='/blog')
 app.register_blueprint(chat.routes.mod, url_prefix='/chat')
-
-## Socket IO connection for chat and playing chess game.
-
-socketio = SocketIO(app)
 
 # Makes chess board easily accessible thorugh terminal
 
