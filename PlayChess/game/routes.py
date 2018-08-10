6706e58973be193bc207634b1ce3a4184d96d402
Shubham Anand <shubham_chess@live.com>
2018-08-10 19:51:45 +0530

    move find_player routine to index page

--- PlayChess/game/routes.py
+++ PlayChess/game/routes.py
@@ -4,33 +4,13 @@ from flask import Blueprint, render_template, url_for, request, session, redirec
 from datetime import timedelta, datetime
 from flask_socketio import send
 
-from .game import Game, get_game, make_game
-from ..utils import decorators
+from ..utils import decorators, exceptions
 
 # Import global variables
 from ..config import *
 from .. import socketio
 
 mod = Blueprint('game', __name__, template_folder='templates')
-
-# Handle game loading here
-@mod.route('/find')
-@decorators.login_required
-def find_players():
-    if not USER_DICT['current_user_' + str(session['username'])].in_game['status']:
-        make_game(session, PLAYERS_QUEUE, GAMES)
-        end_time = (datetime.now() + timedelta(seconds=10)).time()
-        while end_time >= datetime.now().time():
-            game = get_game(session, GAMES)
-            if game:
-                USER_DICT['current_user_' + str(session['username'])].in_game['status'] = True
-                USER_DICT['current_user_' + str(session['username'])].in_game['url'] = game
-                return jsonify({"url": game})
-            time.sleep(1)
-        PLAYERS_QUEUE.remove(session['username'])
-        return jsonify({"url": None})
-    url = USER_DICT['current_user_' + str(session['username'])].in_game['url']
-    return jsonify({"url": url})
 
 ## Game route
 @mod.route('/<game_url>')
@@ -43,5 +23,35 @@ def game(game_url):
         player1 = game.player1
         player2 = game.player2
         return render_template('game.html', game_title=game_title, board=board, player1=player1, player2=player2)
-    return jsonify({"BAD_EXCESS": "ABORT 404"})
+    raise exceptions.GameNotFound("This game doesn't exist!")
 
+@mod.route('/<game_url>/makemove/<move>')
+@decorators.login_required
+def make_move(game_url, move):
+    if GAMES.get(game_url):
+        init_pos, final_pos = move.split('-')[0], move.split('-')[1]
+        return GAMES[game_url].make_move(init_pos, final_pos, session.get('username'))
+    raise exceptions.GameNotFound("This game doesn't exist!")
+
+@mod.route('/<game_url>/generateLegalMoves/<init_pos>')
+@decorators.login_required
+def generate_legal_moves(game_url, init_pos):
+    if GAMES.get(game_url):
+        return GAMES[game_url].generate_legal_moves(init_pos, session.get('username'))
+    raise exceptions.GameNotFound("This game doesn't exist!")
+
+## Add a cryptographic id so that only server can dissolve games
+## Something like /end/<server_id>/<game_url> or better yet 
+## encrypt game urls.
+@mod.route('/<game_url>/end')
+@decorators.login_required
+def end_game(game_url):
+    if GAMES.get(game_url):
+        player1 = GAMES[game_url].player1
+        player2 = GAMES[game_url].player2
+        USER_DICT['current_user_' + player1].in_game['status'] = False
+        USER_DICT['current_user_' + player1].in_game['url'] = None
+        USER_DICT['current_user_' + player2].in_game['status'] = False
+        USER_DICT['current_user_' + player2].in_game['url'] = None
+        GAMES.pop(game_url)
+    raise exceptions.GameNotFound("This game doesn't exist!")
