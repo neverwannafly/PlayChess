8bbd24f26ba7565c433189faccfef3d7d9b8fae8
Shubham Anand <shubham_chess@live.com>
2018-08-10 19:52:51 +0530

    add senderverification and gamenotfound exceptions

--- PlayChess/utils/exceptions.py
+++ PlayChess/utils/exceptions.py
@@ -3,3 +3,9 @@
 class InvalidMoveError(Exception):
     """Is raised when an user makes an invalid move"""
     pass
+
+class SenderCannotBeVerified(Exception):
+    "Is raised when make_move is called by questionable sender"
+
+class GameNotFound(Exception):
+    "Is raised when a game url cannot be found"
