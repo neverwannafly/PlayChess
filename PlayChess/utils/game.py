37c57d3bb4895523044209149134a4d5edd9a423
Shubham Anand <shubham_chess@live.com>
2018-08-10 19:51:30 +0530

    move game.py to utils

--- PlayChess/utils/game.py
+++ PlayChess/utils/game.py
@@ -2,7 +2,10 @@
 
 import random
 
-from ..utils import chessboard
+from functools import wraps
+from flask import jsonify
+
+from ..utils import chessboard, exceptions
 
 class Game:
     
@@ -12,6 +15,7 @@ class Game:
         # Player1 would be white and Player2 would be black!
         self.player1 = None
         self.player2 = None
+        self.moves = 0
         self.spectators = []
 
         self.game_name = game_name
@@ -22,6 +26,32 @@ class Game:
         player1, player2 = self.game_name.split('-')[index], self.game_name.split('-')[int(not index)]
         self.player1 = player1
         self.player2 = player2
+
+    def generate_legal_moves(self, init_pos, sender):
+        self.verify_sender(sender)
+        moves = self.chessboard.generate_legal_moves(init_pos);
+        return jsonify({'moves': moves})
+
+    def make_move(self, init_pos, final_pos, sender):
+        self.verify_sender(sender)
+        try:
+            changes = self.chessboard.make_move(init_pos, final_pos)
+        except exceptions.InvalidMoveError as error:
+            print(error)
+            return jsonify({'success': False})
+        return jsonify({
+        'success': True,
+        'changes': changes,
+    })
+
+    def verify_sender(self, sender):
+        def check_sender(sender, moves, player1, player2):
+            if moves%2 == 0:
+                return sender==player1
+            else:
+                return sender==player2
+        if not check_sender(sender, self.moves, self.player1, self.player2):
+            raise exceptions.SenderCannotBeVerified("Sender Cannot be verified!")
 
 ## Helper functions
 def make_game(session, players_queue, ongoing_games):
