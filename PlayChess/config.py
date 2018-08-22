6ac0216a1044720e902749a8448c23645502faae
Shubham Anand <shubham_chess@live.com>
2018-08-22 19:54:11 +0530

    add terminal colors configs

--- PlayChess/config.py
+++ PlayChess/config.py
@@ -24,3 +24,17 @@ PLAYERS_QUEUE = game_queue.GameQueue()
 EMAIL_PATTERN_COMPILED = regex.compile("^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")
 # Username regex also limits the string to be b/w 5 and 30!
 USERNAME_REGEX = regex.compile("^[a-zA-Z0-9_]{5,30}$")
+
+# Codes for terminal colors
+TERMINAL_COLORS = {
+    'CBOLD': '\33[1m',
+    'CURL': '\33[4m',
+    'CRED': '\33[31m',
+    'CGREEN': '\33[32m',
+    'CYELLOW': '\33[33m',
+    'CBLUE': '\33[34m',
+    'CVIOLET': '\33[35m',
+    'CBEIGE': '\33[36m',
+    'CWHITE': '\33[37m',
+    'CEND': '\033[0m',
+}
