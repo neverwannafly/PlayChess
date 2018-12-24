00d69eab2b3958df349d21aff44d579755e7a883
Shubham Anand <shubham_chess@live.com>
2018-12-24 20:48:54 +0530

    add exceptions for checkmate, draw and out of turn moves

--- PlayChess/utils/exceptions.py
+++ PlayChess/utils/exceptions.py
@@ -4,8 +4,25 @@ class InvalidMoveError(Exception):
     """Is raised when an user makes an invalid move"""
     pass
 
+class SideNotAuthorizedToMakeMove(Exception):
+    """Is raised when idle side (currently not having a turn) makes a move"""
+    pass
+
+class Checkmate(Exception):
+    """Is raised when one of the players is checkmated"""
+    def __init__(self):
+        self.result = None
+    
+class Draw(Exception):
+    """Is raised when a drawing condition is met"""
+    def __init__(self):
+        self.cause = None
+        self.result = None
+
 class SenderCannotBeVerified(Exception):
-    "Is raised when make_move is called by questionable sender"
+    """Is raised when make_move is called by questionable sender"""
+    pass
 
 class GameNotFound(Exception):
-    "Is raised when a game url cannot be found"
+    """Is raised when a game url cannot be found"""
+    pass
