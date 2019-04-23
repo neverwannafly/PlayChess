b4e0986d5340b08e8ee6f5fce089fc0c6f27c327
Shubham Anand <shubham_chess@live.com>
2019-04-23 19:08:23 +0530

    fix db object reference

--- PlayChess/database/__init__.py
+++ PlayChess/database/__init__.py
@@ -4,8 +4,8 @@ from pymongo import MongoClient
 URL = os.environ.get("DATABASE_URL", None)
 if URL:
     client = MongoClient(URL)
+    db = client.playchesswebsite
 else:
     configurations = config.configurations
     client = MongoClient('mongodb://' + configurations['_USERNAME'] + ':' + configurations['_PASSWORD'] + '@ds151169.mlab.com:51169/chess_database', serverSelectionTimeoutMS=10000)
-    
-db = client.chess_database # Establishes connection to mlab database!
+    db = client.chess_database # Establishes connection to mlab database!
