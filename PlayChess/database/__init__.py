bac07a15bb0891b99b59402e5b3ba9be9934ae45
Shubham Anand <shubham_chess@live.com>
2019-04-23 19:07:42 +0530

    seperate production and development databases

--- PlayChess/database/__init__.py
+++ PlayChess/database/__init__.py
@@ -1,7 +1,11 @@
 from . import config
 from pymongo import MongoClient
 
-configurations = config.configurations
-
-client = MongoClient('mongodb://' + configurations['_USERNAME'] + ':' + configurations['_PASSWORD'] + '@ds151169.mlab.com:51169/chess_database', serverSelectionTimeoutMS=10000)
+URL = os.environ.get("DATABASE_URL", None)
+if URL:
+    client = MongoClient(URL)
+else:
+    configurations = config.configurations
+    client = MongoClient('mongodb://' + configurations['_USERNAME'] + ':' + configurations['_PASSWORD'] + '@ds151169.mlab.com:51169/chess_database', serverSelectionTimeoutMS=10000)
+    
 db = client.chess_database # Establishes connection to mlab database!
--- PlayChess/database/config.py
+++ PlayChess/database/config.py
@@ -1,4 +1,4 @@
-# Contains the database username and pass
+# Contains the database username and pass for develepment database
 configurations = {
     '_USERNAME' : 'neverwannafly',
     '_PASSWORD' : '10p17fs0008',
