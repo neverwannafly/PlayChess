c505034a2bd76a1ae0d3f4dd5e651bd852b38897
Shubham Anand <shubham_chess@live.com>
2019-05-24 02:10:21 +0530

    allow auto reload for gunicorn

--- Scripts/rundev.sh
+++ Scripts/rundev.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-gunicorn --threads 12 -w 1 run:app || echo "\033[91mMake sure you've Gunicorn installed or you're in your virtual environment!\033[0m"
+gunicorn --reload --threads 12 -w 1 run:app || echo "\033[91mMake sure you've Gunicorn installed or you're in your virtual environment!\033[0m"
