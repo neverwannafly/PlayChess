fbc929d255ae95c1605082dd0921883353d40012
Shubham Anand <shubham_chess@live.com>
2019-06-23 16:56:47 +0530

    add testing for castling and checks

--- Tests/test_chessboard_moves.py
+++ Tests/test_chessboard_moves.py
@@ -53,12 +53,43 @@ def test_castling(client):
 
     # Check long castle
     login(client)
+    client.get('/board/makemove/d2-d4')
+    client.get('/board/makemove/d7-d5')
+    client.get('/board/makemove/b1-c3')
+    client.get('/board/makemove/b8-c6')
+    client.get('/board/makemove/c1-f4')
+    client.get('/board/makemove/c8-f5')
+    client.get('/board/makemove/d1-d2')
+    client.get('/board/makemove/d8-d7')
+    white_castle = client.get('/board/makemove/e1-c1')
+    black_castle = client.get('/board/makemove/e8-c8')
+    assert b'"success":true' in white_castle.data
+    assert b'"success":true' in black_castle.data
+
+    # No castling in check
+    client.get('/board/reset?fen=rnbq1rk1/pppp1ppp/5n2/4p3/3PP3/2b2N2/PP2BPPP/RNBQK2R w KQ - 1 6')
+    white_castle = client.get('/board/makemove/e1-g1')
+    assert b'"success":false' in white_castle.data
+
+    # No castling when obstacle in the way
+    client.get('/board/reset?fen=rn1qkbnr/ppp1pppp/3p4/1b6/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 1 4')
+    white_castle = client.get('/board/makemove/e1-g1')
+    assert b'"success":false' in white_castle.data
+
+    # Castling couldnt lead to king being in check
+    client.get('/board/reset?fen=r1bqk1nr/ppp2ppp/2np4/2b1p3/2B1PP2/5N2/PPPP2PP/RNBQK2R w KQkq - 1 5')
+    white_castle = client.get('/board/makemove/e1-g1')
+    assert b'"success":false' in white_castle.data
     
 def test_checks(client):
     login(client)
     client.get('/board/reset?fen=rn1qkb1r/ppp2Bpp/3p1n2/4N3/4P3/2N5/PPPP1PPP/R1BbK2R b KQkq - 1 6')
     king_move = client.get('/board/makemove/e8-e7')
     assert b'"success":true' in king_move.data
+
+    client.get('/board/reset?fen=rn1qkbnr/ppp1pppp/3p4/1b6/4P3/5N2/PPPP1PPP/RNBQK2R w KQkq - 1 4')
+    king_move = client.get('/board/makemove/e1-e2')
+    assert b'"success":false' in king_move.data
 
 def test_fen_sanity(client):
     login(client)
