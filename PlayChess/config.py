38076e0bd1a0bc59ebd77600dde15252b83611a7
Shubham Anand <shubham_chess@live.com>
2019-08-09 14:03:11 +0530

    make server run in local/offline mode

--- PlayChess/config.py
+++ PlayChess/config.py
@@ -15,6 +15,8 @@ USER_DICT = {}
 GAMES = {}
 ADMIN_DICT = {}
 
+IS_LOCAL = False
+
 # Keeps track of players finding matches.
 PLAYERS_QUEUE = game_queue.GameQueue()
 
--- PlayChess/database/__init__.py
+++ PlayChess/database/__init__.py
@@ -1,14 +1,19 @@
 import os
 from ..utils import exceptions
+from ..config import IS_LOCAL
 
 from pymongo import MongoClient
 
+URL_LOCAL = os.environ.get("DATABASE_LOCAL_URI", None)
 URL_PROD = os.environ.get("DATABASE_URL", None)
 URL_DEV = os.environ.get("DATABASE_URL_DEV", None)
 
 if URL_PROD:
     client = MongoClient(URL_PROD, serverSelectionTimeoutMS=10000)
     db = client.playchesswebsite
+elif URL_LOCAL and IS_LOCAL:
+    client = MongoClient(URL_LOCAL, serverSelectionTimeoutMS=10000)
+    db = client.chess_database
 elif URL_DEV:
     client = MongoClient(URL_DEV, serverSelectionTimeoutMS=10000)
     db = client.chess_database
