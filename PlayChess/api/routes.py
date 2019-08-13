c8da049d89c7dbe48a0a9351a08f3278065619d5
Shubham Anand <shubham_chess@live.com>
2019-08-13 20:07:41 +0530

    fix memory leak issue

--- PlayChess/api/routes.py
+++ PlayChess/api/routes.py
@@ -16,39 +16,34 @@ def getToken():
 
 @mod.route('/stockfish/', methods=['GET'])
 def getEngineEval():
+    if token.validate_token(request.args.get('token', False)):
+        __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+        __filename__ = 'stockfish-prod' if os.environ.get("Production", False) else 'stockfish-dev'
 
-    # Temporarily stop Engine Eval
+        # Assign game Position
+        with chess.engine.SimpleEngine.popen_uci(os.path.join(__location__, __filename__)) as engine:
+            try:
+                fen_notation = request.args.get('fen_notation', "")
+                board = chess.Board(fen_notation)
+            except ValueError:
+                return jsonify({
+                    "fen_notation_error": "Please supply a valid fen notation",
+                })
+            strength = request.args.get('strength', 1)
+            time = min(1, 0.01 * 10 ** int(strength))
+            res = engine.play(board, chess.engine.Limit(time=time, depth=20))
+            info = engine.analyse(board, chess.engine.Limit(time=time, depth=20))
+
+            engine.close()
+
+            return jsonify({
+                "best_move": str(res.move), 
+                "ponder": str(res.ponder),
+                "evaluation": str(info["score"].white()),
+            })
     return jsonify({
-        "evaluation": "=0",
+        "access_token_error": "There seems to be a problem with your access token",
     })
-
-    # if token.validate_token(request.args.get('token', None)):
-    #     __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
-    #     if os.environ.get("Production", None):
-    #         engine = chess.engine.SimpleEngine.popen_uci(os.path.join(__location__, 'stockfish-prod'))
-    #     else:
-    #         engine = chess.engine.SimpleEngine.popen_uci(os.path.join(__location__, 'stockfish-dev'))
-    #     # Assign game Position
-    #     try:
-    #         fen_notation = request.args.get('fen_notation', "")
-    #         board = chess.Board(fen_notation)
-    #     except ValueError:
-    #         return jsonify({
-    #             "fen_notation_error": "Please supply a valid fen notation",
-    #         })
-    #     strength = request.args.get('strength', 1)
-    #     time = min(1, 0.01 * 10 ** int(strength))
-    #     res = engine.play(board, chess.engine.Limit(time=time, depth=20))
-    #     info = engine.analyse(board, chess.engine.Limit(time=time, depth=20))
-
-    #     return jsonify({
-    #         "best_move": str(res.move), 
-    #         "ponder": str(res.ponder),
-    #         "evaluation": str(info["score"].white()),
-    #     })
-    # return jsonify({
-    #     "access_token_error": "There seems to be a problem with your access token",
-    # })
 
 # Send start date and end date as the string "None" if you want to fetch all events
 @mod.route('/events/<startDate>/<endDate>', methods=['GET'])
